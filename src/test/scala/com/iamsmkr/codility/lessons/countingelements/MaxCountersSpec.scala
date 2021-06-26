cd4c4addab4aea50e56c7da612ea6f7e0c0d0239
iamsmkr <mailtoshivamk@gmail.com>
2021-06-26 09:41:48 +0000

    max counters

---
+++ src/test/scala/com/iamsmkr/codility/lessons/countingelements/MaxCountersSpec.scala
@@ -0,0 +1,20 @@
+package com.iamsmkr.codility.lessons.countingelements
+
+import org.scalatest.matchers.should
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class MaxCountersSpec extends AnyPropSpec with should.Matchers with ScalaCheckPropertyChecks {
+  property("MaxCounters should be computed max counters correctly") {
+    val results =
+      Table(
+        ("counters", "arr", "result"),
+        (1, Array(2, 1, 1, 2, 1), Array(3)),
+        (5, Array(3, 4, 4, 6, 1, 4, 4), Array(3, 2, 2, 4, 2))
+      )
+
+    forAll(results) { (x, arr, result) =>
+      MaxCounters.solution(x, arr) should equal(result)
+    }
+  }
+}
