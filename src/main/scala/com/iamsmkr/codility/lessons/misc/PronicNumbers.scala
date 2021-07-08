e7b01393f01cf310744d6ee5d1a7380af89a79be
iamsmkr <mailtoshivamk@gmail.com>
2021-07-08 19:21:23 +0000

    notes

--- src/main/scala/com/iamsmkr/codility/lessons/misc/PronicNumbers.scala
+++ src/main/scala/com/iamsmkr/codility/lessons/misc/PronicNumbers.scala
@@ -1,5 +1,20 @@
 package com.iamsmkr.codility.lessons.misc
 
+/**
+ * Given two integers A and B, return the number of integers from the range A..B which can be expressed as the product of
+ * two consecutive integers, that is X *(X + 1) which is also known as Pronic Number.
+ *
+ * Example 1 :
+ * A = 6 and B = 20, the function should return 3, These integers are 6 = 2 * 3, 12 = 3 * 4 and 20 = 4* 5
+ *
+ * Example 2 :
+ * A = 21 and B = 29, the function should return 0
+ *
+ * Assumptions:
+ * A and B are integers within the range [1...1000,000,000]
+ * A <= B
+ */
+
 object PronicNumbers {
   def solution(a: Int, b: Int): Int = {
     var count = 0
